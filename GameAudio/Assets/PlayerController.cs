c79efb7295fb0c479c40f9630e92807520242223
Madeleine Johnston <M.Johnston1@student.gsa.ac.uk>
2019-03-20 12:54:14 +0000

    Simple Scene Set up
    Just got planets, particle effects and a moveable cube.
    Will work on gravity/camera following player.
    Audio - now more lighter/quirky feel thn horror.

---
+++ GameAudio/Assets/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    private float xForce;
+    private float zForce;
+    private Vector3 force;
+
+    private float pitch = 0.0F;
+    private float yaw = 0.0F;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        //takes information about positions from the current scene
+        //to be used in movement calculations
+        xForce = Input.GetAxis("Horizontal");
+        zForce = Input.GetAxis("Vertical");
+
+        pitch -= Input.GetAxis("Mouse Y");
+        yaw += Input.GetAxis("Mouse X");
+
+        //stops the player from rolling on a slope when no longer pressing the keys
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            rb.Sleep();
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            rb.Sleep();
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            rb.Sleep();
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            rb.Sleep();
+        }
+
+    }
+
+    void LateUpdate()
+    {
+        //applies force
+        force = new Vector3((xForce * 2), 0.0F, (zForce * 2));
+
+        // rotate object to face mouse direction
+        rb.transform.localEulerAngles = new Vector3(0.0f, yaw, 0.0F);
+
+        // move object in facing direction relative to local (AddRelative) not world (AddForce) coordinates
+        rb.AddRelativeForce(force);
+
+    }
+
+}
