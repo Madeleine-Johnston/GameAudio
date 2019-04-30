d853330cce0c2ad2ba46304c5b70c703c5b88b64
ibariabani <40428662+ibariabani@users.noreply.github.com>
2019-05-01 00:25:47 +0100

    Added UI

---
+++ GameAudio/Assets/Scripts/UIController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+
+    //public string nextScene;
+    public Animator cameraAnim;
+    public GameObject panel;
+
+    public bool isOn;
+
+    void Start()
+    {
+       cameraAnim = GetComponent<Animator>();
+    }
+    public void Quit()
+    {
+        //SceneManager.LoadScene();
+        Debug.Log("Quitting");
+        Application.Quit();
+    }
+
+    public void ChangeScene(string nextScene){
+		Debug.Log("Changing to scene: " + nextScene);
+        SceneManager.LoadScene(nextScene);
+        //Invoke("DelayedAction", 3);
+	}	
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P)){
+            Pause();
+            panel.SetActive(!isOn);
+        }
+    }
+
+
+    //credit to Vincent Quarles for pause piece of code
+    //https://www.sitepoint.com/adding-pause-main-menu-and-game-over-screens-in-unity/
+
+    public void Pause(){
+        if(Time.timeScale == 1)
+		{
+			Time.timeScale = 0;
+		} else if (Time.timeScale == 0){
+			Time.timeScale = 1;	
+		}
+    }
+
+    /*public void CameraZoom(){
+        cameraAnim.SetTrigger("CameraZoom");
+    }*/
+}
