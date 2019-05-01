9a6d6468e9d74c12c81c967f91a2fbb0fb232b77
Madeleine Johnston <M.Johnston1@student.gsa.ac.uk>
2019-05-01 19:24:18 +0100

    ibaris final

---
+++ GameAudio/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator cameraAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+       cameraAnim = GetComponent<Animator>();
+    }
+
+    public void CameraZoom(){
+        cameraAnim.SetTrigger("CameraZoom");
+    }
+}
