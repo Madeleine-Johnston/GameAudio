6252ed63a997375a8aa9ae165b58aa9928fa1139
Madeleine Johnston <M.Johnston1@student.gsa.ac.uk>
2019-05-02 12:10:31 +0100

    update

---
+++ GameAudio/Assets/Scripts/VolumeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeController : MonoBehaviour
+{
+
+    public AudioMixer mixer;
+
+    public void musicVol(float musiclevel)
+    {
+        mixer.SetFloat("MusicVol", musiclevel);
+    }
+
+    public void masterVol(float masterlevel)
+    {
+        mixer.SetFloat("MusicVol2", masterlevel);
+    }
+
+    public void SFXVol(float sfxlevel)
+    {
+        mixer.SetFloat("MusicVol2", sfxlevel);
+    }
+}
